5a34b4848a2b6a079eaee1fba6fd5d4d462a7c37
joelyakubu269 <joelyakubu269@gmail.com>
2026-03-05 01:43:39 +0000

    Create cliApp2.go
    creating a simple cli app which json files which store the values

---
+++ cliApp2.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"encoding/json"
+	"io/ioutil"
+)
+
+type task struct {
+	ID          int     `json:"id"`
+	Description string   `json:"description"`
+	Status      string    `json:"status"`
+}
+
+var nextID = 1
+var tasks []task
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("please enter a command")
+		return
+	}
+	command := os.Args[1]
+	switch command {
+	case "add":
+		if len(os.Args) < 3 {
+			fmt.Println("There must be a command")
+			return
+		}
+		description := strings.Join(os.Args[2:], " ")
+		add(description)
+	case "delete":
+		if len(os.Args) < 3 {
+			fmt.Println("please enter a command")
+			return
+		}
+		idstr := os.Args[2]
+		deleteTask(idstr)
+	case "update":
+		if len(os.Args) < 4 {
+			fmt.Println("add a new description")
+			return
+		}
+		idstr := os.Args[2]
+		newdesc := strings.Join(os.Args[3:], " ")
+		update(idstr, newdesc)
+	case "mark":
+		if len(os.Args) < 4 {
+			fmt.Println("not enough commands")
+			return
+		}
+		idstr := os.Args[2]
+		newStatus := strings.Join(os.Args[3:], " ")
+		mark(idstr, newStatus)
+	case "list":
+		listAll()
+	case "list-not-done":
+		listNotDone()
+	case "list-in-progress":
+		listTaskInprogress()
+	default:
+		fmt.Println("Unknown command. Use add, delete, update, mark, list, ...")
+
+	}
+}
+
+func add(description string) {
+	newTask := task{
+		ID:          nextID,   
+		Description: description,
+		Status:      "todo",
+	}
+	nextID++
+	tasks = append(tasks, newTask)
+	fmt.Println("task successfully added")
+	fmt.Printf("Added task: ID:%d, Description:%s, Status:%s\n", newTask.ID, newTask.Description, newTask.Status)
+}
+func deleteTask(idstr string) {
+	var update []task
+	id, err := strconv.Atoi(idstr)
+	if err != nil {
+		fmt.Println("There was an error trying to convert")
+	}
+	found := false
+	for _, r := range tasks {
+		if r.ID != id {
+			update = append(update, r)
+		} else {
+			found = true
+		}
+
+	}
+	if !found {
+		fmt.Println("task does not have an id")
+		return
+	}
+	tasks = update
+	fmt.Println("tasks successfully deleted")
+	listAll()
+}
+func update(idstr, newDesc string) {
+	id, err := strconv.Atoi(idstr)
+	if err != nil {
+		fmt.Println(" There was an error trying to convert")
+		return
+	}
+	found := false
+	for i, r := range tasks {
+		if r.ID == id {
+			tasks[i].Description = newDesc
+			found = true
+		}
+	}
+	if !found {
+		fmt.Println("ID was not found")
+		return
+	}
+	fmt.Println("Task successfully updated")
+	listAll()
+}
+func mark(idstr, newStatus string) {
+	id, err := strconv.Atoi(idstr)
+	if err != nil {
+		fmt.Println("Error in converting to integer")
+		return
+	}
+	found := false
+	validStatuses := map[string]bool{"todo": true, "in-progress": true, "done": true}
+	if !validStatuses[newStatus] {
+		fmt.Println("Invalid status. Use: todo, in-progress, done")
+		return
+	}
+	for i, r := range tasks {
+		if r.ID == id {
+			tasks[i].Status = newStatus
+			found = true
+			break
+		}
+
+	}
+	if !found {
+		fmt.Println("ID not found")
+		return
+	}
+	fmt.Println("status changed")
+}
+func listAll() {
+	for _, r := range tasks {
+		fmt.Printf("ID:%d, Description:%s,Status:%s\n", r.ID, r.Description, r.Status)
+	}
+}
+func listNotDone() {
+	var notDone []task
+
+	for _, r := range tasks {
+		if r.Status == "todo" || r.Status == "in-progress" {
+			notDone = append(notDone, r)
+
+		}
+	}
+	if len(notDone) == 0 {
+		fmt.Println("no task found that are not done")
+		return
+	}
+	for _, t := range notDone {
+		fmt.Printf("ID:%d, Description:%s, Status:%s\n", t.ID, t.Description, t.Status)
+	}
+
+}
+func listTaskInprogress() {
+	var taskInprogress []task
+
+	for _, r := range tasks {
+		if r.Status == "in-progress" {
+			taskInprogress = append(taskInprogress, r)
+
+		}
+	}
+	if len(taskInprogress) == 0 {
+		fmt.Println("no tasks found that are in-progress")
+		return
+	}
+	for _, t := range taskInprogress {
+		fmt.Printf("ID:%d, Description:%s, Status:%s\n", t.ID, t.Description, t.Status)
+	}
+
+}
